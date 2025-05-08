b30db08a8ffe95d35c2ec685275dd2707b16c8c9
pennjay237 <pennjude31@gmail.com>
2025-05-08 08:54:50 +0100

    add

--- add-salary.js
+++ add-salary.js
@@ -2,6 +2,29 @@ let salaries = {
     John: 100,
     Ann: 160,
     Pete: 130
-};
-
-console.log(sumSalaries(salaries)); 
+  };
+  
+  const sumSalaries = (salaries) => {
+    let sum = 0;
+  
+    for (let name in salaries) {
+      switch (name) {
+        case "John":
+          sum += salaries[name];
+          break;
+        case "Ann":
+          sum += salaries[name];
+          break;
+        case "Pete":
+          sum += salaries[name];
+          break;
+        default:
+          break;
+      }
+    }
+  
+    return sum;
+  };
+  
+  console.log(sumSalaries(salaries)); // Output: 390
+  
